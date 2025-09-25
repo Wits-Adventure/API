c347056bb689144b72fa00e6bc41c70a6f953a7d
tetelochuene <tetelolchuene@gmail.com>
2025-09-25 17:58:18 +0200

    testing api

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html']
+  collectCoverageFrom: [
+    'functions/auth.js',
+    'routes/users.js',
+    'routes/upload.js',
+    '!**/node_modules/**'
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
+  testMatch: [
+    '<rootDir>/tests/**/*.test.js',
+    '<rootDir>/functions/**/*.test.js',
+    '<rootDir>/routes/**/*.test.js'
+  ]
 };
